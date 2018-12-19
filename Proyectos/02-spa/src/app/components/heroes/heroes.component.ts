6c71fd90f774ed01c371b0a61b0d04c85e2f2879
Jorge Prados <jprados@serbatic.es>
2018-12-19 09:10:25 +0100

    Servicios Proyecto SPA

--- Proyectos/02-spa/src/app/components/heroes/heroes.component.ts
+++ Proyectos/02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HeroesService} from '../../servicios/heroes.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor( private _heroesService:HeroesService ) { }
 
   ngOnInit() {
   }
