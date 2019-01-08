b805c918fc4c360087491bfbc6c5c5927f68ff15
Jorge Prados <jprados@serbatic.es>
2019-01-08 17:26:15 +0100

    Creada página detalle heroe y servicio buscador

--- Proyectos/02-spa/src/app/components/heroe/heroe.component.ts
+++ Proyectos/02-spa/src/app/components/heroe/heroe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService, Heroe } from '../../servicios/heroes.service'
 
 @Component({
   selector: 'app-heroe',
@@ -7,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe:Heroe;
+  constructor(  private activatedRoute: ActivatedRoute,
+                private _heroesService:HeroesService      
+    ) {
+    this.activatedRoute.params.subscribe(params =>{
+      //console.log( params['id'] );
+      this.heroe = this._heroesService.getHeroe(params['id']);
+      console.log(this.heroe);
+    })
+  }
 
   ngOnInit() {
   }
